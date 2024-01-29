cbc022fcb4dbe8164d4f0b3b50067a6fac5c810b
Jungbae Kim <alex@by-softcorp.com>
2024-01-29 15:31:13 +0900

    mod : remove test tags

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-1706509226';
+const cacheName = 'chirpy-1706509811';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
