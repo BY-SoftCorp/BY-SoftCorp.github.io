529bbeadca8c5868aebf8c89533ae26148493da2
Jungbae Kim <alex@by-softcorp.com>
2024-02-02 11:16:35 +0900

    add : some posting with legacies

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-1706509811';
+const cacheName = 'chirpy-1706840157';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
