1301d4d5278ad39594cad0d8bb721e188a9db347
Jungbae Kim <alex@by-softcorp.com>
2024-03-26 12:01:39 +0900

    add : react mvvm pattern

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-1706840157';
+const cacheName = 'chirpy-1711419417';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
