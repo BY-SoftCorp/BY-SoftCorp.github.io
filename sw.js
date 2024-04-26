b3278c172f8042f56f74366b0f891454a69230af
Jungbae Kim <alex@by-softcorp.com>
2024-04-26 10:59:57 +0900

    add : reuse viewmodel

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-1711703075';
+const cacheName = 'chirpy-1714096702';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
